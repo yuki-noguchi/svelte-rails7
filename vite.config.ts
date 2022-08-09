88a02af89adf1ac511fc7888a075698553ad4d6d
yuki-noguchi <y.n.10191@gmail.com>
2022-08-09 23:46:41 +0900

    add vite fullreload plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import { defineConfig } from "vite";
+import RubyPlugin from "vite-plugin-ruby";
+import FullReload from "vite-plugin-full-reload";
 
 export default defineConfig({
-  plugins: [
-    RubyPlugin(),
-  ],
-})
+  plugins: [RubyPlugin(), FullReload(["config/routes.rb", "app/views/**/*"])],
+});
